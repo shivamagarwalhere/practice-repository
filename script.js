ead1ffed36900b0b300e5b73fc7b1411bf986a33
Shivam Agarwal <shivam2002tintin@gmail.com>
2024-02-08 22:13:02 +0530

    completed project 1

--- script.js
+++ script.js
@@ -3,9 +3,20 @@ rect.addEventListener("mousemove", function(details){
     var rectanglelocation = rect.getBoundingClientRect();
     var insiderectval = details.clientX-rectanglelocation.left;
     if (insiderectval<rectanglelocation.width/2){
-        console.log("left");
+        var redcolor = gsap.utils.mapRange(0, rectanglelocation.width/2, 255, 0, insiderectval);
+        gsap.to(rect, {
+            backgroundColor: `rgb(${redcolor},0,0)`,
+            ease: Power4,
+        });
     }
     else{
-        console.log("right");
+        var bluecolor = gsap.utils.mapRange(rectanglelocation.width/2, 400, 0, 255, insiderectval);
+        gsap.to(rect, {
+            backgroundColor: `rgb(0,0,${bluecolor})`,
+            ease: Power4,
+        });
     }
 });
+
+rect.addEventListener("mouseleave", function(){
+    gsap.to(rect, {backgroundColor: "white"})});
