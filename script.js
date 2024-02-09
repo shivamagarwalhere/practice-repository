c8222187082e3375c361c500da9c60a1885dec5c
Shivam Agarwal <shivam2002tintin@gmail.com>
2024-02-09 10:52:07 +0530

    practice2 added

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ var rect = document.querySelector("#center");
 rect.addEventListener("mousemove", function(details){
     var rectanglelocation = rect.getBoundingClientRect();
     var insiderectval = details.clientX-rectanglelocation.left;
+    
     if (insiderectval<rectanglelocation.width/2){
         var redcolor = gsap.utils.mapRange(0, rectanglelocation.width/2, 255, 0, insiderectval);
         gsap.to(rect, {
