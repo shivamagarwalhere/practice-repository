3832b3d3d48f2b83a2113a12f3cfb1c423dd10aa
shivamagarwalhere <shivam2002tintin@gmail.com>
2024-06-09 23:36:32 +0530

    async js/dom

--- placement-training/dom-manipulation/script.js
+++ placement-training/dom-manipulation/script.js
@@ -1,23 +1,51 @@
-// document.getElementById('shivam').innerHTML = "Shivam"
+// // document.getElementById('shivam').innerHTML = "Shivam"
 
-// const listItems = document.getElementsByClassName('list-group-item');
-// for (let i = 0; i < listItems.length; i++){
-//     listItems[i].innerHTML = `Task ${i+1}`;
+// // const listItems = document.getElementsByClassName('list-group-item');
+// // for (let i = 0; i < listItems.length; i++){
+// //     listItems[i].innerHTML = `Task ${i+1}`;
+// // }
+
+// //create a div
+// // let newDiv = document.createElement('div');
+// // newDiv.className = 'hello';
+// // newDiv.setAttribute('title', 'Hello Div');
+// // const newDivText = document.createTextNode('HelloO SHivam');
+// // console.log(newDivText);
+// // newDiv.appendChild(newDivText);
+
+// // let container = document.querySelector('header .container');
+// // let h1 = document.querySelector('header h1');
+
+// // console.log(newDiv);
+
+// // newDiv.style.fontsize = '30px';
+
+// // container.insertBefore(newDiv, h1);
+
+// var button = document.getElementById('button');
+// button.addEventListener('click', runEvent);
+// button.addEventListener('mouseenter', runEvent);
+// button.addEventListener('mouseleave', runEvent);
+
+// function runEvent(e){
+//     console.log('EVENT TYPE: '+e.type);
 // }
+ let form = document.getElementById('addForm')
+let itemList = document.getElementById('items');
 
-//create a div
-let newDiv = document.createElement('div');
-newDiv.className = 'hello';
-newDiv.setAttribute('title', 'Hello Div');
-const newDivText = document.createTextNode('HelloO SHivam');
-console.log(newDivText);
-newDiv.appendChild(newDivText);
+//form submit event
+form.addEventListener('submit', addItem)
 
-let container = document.querySelector('header .container');
-let h1 = document.querySelector('header h1');
+//add a item
+function addItem(e){
+    e.preventDefault();
 
-console.log(newDiv);
+    //Get Input Value
+    let newItem = document.getElementById('item');
 
-newDiv.style.fontsize = '30px';
+    //Create a new li element
+    let li = document.createElement('li');
 
-container.insertBefore(newDiv, h1);
+    //add a class
+    
+}
